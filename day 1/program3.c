2555fed97bf804c764cc2bf8add375cef34dd8cb
vikrampratap778-web <vikrampratap778@gmail.com>
2026-06-17 14:36:46 +0530

    Add files via upload

---
+++ day 1/program3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d = %d", n, fact);
+
+    return 0;
+}
